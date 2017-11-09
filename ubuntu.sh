2bed929f1372247f67e5350bd59acb8b6b15a5fb
Damian Chojna <d.chojna@hbq.pl>
2017-11-09 19:38:49 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,8 +4,16 @@
   adduser hbq
   adduser hbq sudo
   
-  #nodejs
+  # APT UPDATE
+  sudo apt-get update
+  
+  #NODEJS
   curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
   sudo apt-get install -y nodejs
   npm i pm2 -g
   setcap 'cap_net_bind_service=+ep' /usr/bin/node #ustawienie mozliwosci otwierania portow ponizej 1024
+  
+  #NGINX
+  sudo apt-get install nginx -y
+  sudo apt-get install mysql-server
+  
